95ff19aa334cd6492e9b5adb2b335e2959ce1c1e
Hyuyeong <choibeny@gmail.com>
2024-12-04 09:02:15 +1300

    Category CRUD

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using MVC.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(opt =>
+    {
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+        opt.UseSqlite(connectionString);
+    });
 
 var app = builder.Build();
 
