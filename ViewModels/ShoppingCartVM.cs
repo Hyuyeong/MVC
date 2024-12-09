89462b37b753386cdaa6547721067a4041cb6707
Hyuyeong <choibeny@gmail.com>
2024-12-09 21:11:07 +1300

    update ShoppingCart

---
+++ ViewModels/ShoppingCartVM.cs
@@ -0,0 +1,10 @@
+using System;
+using MVC.Models;
+
+namespace MVC.ViewModels;
+
+public class ShoppingCartVM
+{
+    public IEnumerable<ShoppingCart> ShoppingCartList { get; set; }
+    public double OrderTotal { get; set; }
+}
